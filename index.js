4c90b2948f478fa965935cd7b9e589334ad983c7
zain huda <mhuda@uwo.ca>
2018-11-09 13:43:54 -0500

    heroku specific deployment

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ app.get('/', (req, res) => {
 });
 
 
-// tells express to tell node to listen to port 5000
-app.listen(5000);
+// this sets our dynamic PORT, from underlying environment
+const PORT =  process.env.PORT || 5000;
+// tells express to tell node to listen to port
+app.listen(PORT);
