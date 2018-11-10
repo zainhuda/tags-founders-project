a969a80530f2e63ef69781da8d1b285806f97fb5
zain huda <mhuda@uwo.ca>
2018-11-09 23:13:38 -0500

    oauth refactor

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
-    }
-  )
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 // this sets our dynamic PORT, from underlying environment
 const PORT =  process.env.PORT || 5000;
 // tells express to tell node to listen to port
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,9 @@
-// use for Oauth
+const passport = require('passport');
+
+module.exports = (app) => {
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
