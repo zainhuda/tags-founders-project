f69ea2e730038bb601c066974e2d52b4fdd61aba
Aradhya-B <37886623+Aradhya-B@users.noreply.github.com>
2018-11-17 18:06:16 -0500

    aradhya commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,14 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-        <h2>yeet this yeast</h2>
+        <h2>Aradhya knows how to code, watch out</h2>
     );
   }
 }
 
 export default App;
+/*
+git add .
+git commmit -m "message"
+git push
+*/
