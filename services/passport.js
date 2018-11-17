67de021021a80940408da8556d52f0e1608cae85
Gareth <gareth.kh.lau@gmail.com>
2018-11-17 18:09:22 -0500

    send it

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+//turns user to id
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+//turns an id to a user
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +25,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
-    }
-  )
+        User.findOne({ googleID: profile.id }).then((existingUser) => {
+            if (existingUser) {
+                //user already exists
+                done(null, existingUser);
+            }
+            else {  //create new user
+                new User({googleID: profile.id}).save().then(user => done(null, user));
+            }
+        })
+            console.log('accessToken', accessToken);
+            console.log('refresh token', refreshToken);
+            console.log('profile', profile);
+        }
+    )
 );
