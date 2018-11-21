87184fce03046a10d8e312aa1ae802c402bc2c05
Matthew <matthew.danics@tapmango.com>
2018-11-20 20:11:27 -0500

    add header to directory w searchbar

--- client/src/components/employeeDirectory.jsx
+++ client/src/components/employeeDirectory.jsx
@@ -1,17 +1,22 @@
-import React, {Component} from 'react';
-import {Col, Button, Row} from "react-bootstrap";
+import React, { Component } from "react";
+import { Col, Row} from "react-bootstrap";
 import ProfileSummary from "./profileSummary";
+import Header from "./header";
 
 class EmployeeDirectory extends Component {
   render() {
     const items = [...Array(100)].map((val, i) =>
-      <Col lg={2} style={{padding: "4px"}}>
+      <Col lg={2} style={{ padding: "4px" }}>
         <ProfileSummary name={"John Doe"} position={"Projects"}/>
       </Col>);
 
     return (
-      <Col lg={9} className="listingContent">
+      <Col lg={9}>
+        <div className="listingContent">
+
+        <Header/>
         <Row> {items} </Row>
+        </div>
       </Col>
     );
   }
