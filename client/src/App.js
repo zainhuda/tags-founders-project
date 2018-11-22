d1da19ce51dcfd1e2a6850324db667d21874f849
Matthew <matthew.danics@tapmango.com>
2018-11-21 23:16:27 -0500

    removed stupidness

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,6 @@ class App extends Component {
 
   constructor(props){
     super(props)
-
-  const routes = [
-    {
-      path: "/",
-      exact: true,
-      sidebar: () => <div>home!</div>,
-      main: () => <h2>Home</h2>
-    },
-    {
-      path: "/bubblegum",
-      sidebar: () => <div>bubblegum!</div>,
-      main: () => <h2>Bubblegum</h2>
-    },
-    {
-      path: "/shoelaces",
-      sidebar: () => <div>shoelaces!</div>,
-      main: () => <h2>Shoelaces</h2>
-    }
-];
-
-
   }
 
   render() {
