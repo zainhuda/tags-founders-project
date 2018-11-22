81b7793372c7528e5b82d1b2eec2c0dc86b435ed
Matthew <matthew.danics@tapmango.com>
2018-11-21 19:59:15 -0500

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,46 @@ import { Button, Col, Grid } from "react-bootstrap";
 import Sidebar from "./components/sidebar";
 import Header from "./components/header";
 import EmployeeDirectory from "./components/employeeDirectory";
+import {Route, BrowserRouter} from "react-router-dom";
+import ProfilePage from "./components/profilePage";
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+  const routes = [
+    {
+      path: "/",
+      exact: true,
+      sidebar: () => <div>home!</div>,
+      main: () => <h2>Home</h2>
+    },
+    {
+      path: "/bubblegum",
+      sidebar: () => <div>bubblegum!</div>,
+      main: () => <h2>Bubblegum</h2>
+    },
+    {
+      path: "/shoelaces",
+      sidebar: () => <div>shoelaces!</div>,
+      main: () => <h2>Shoelaces</h2>
+    }
+];
+
+
+  }
+
   render() {
     return (
-      <Grid className="App" fluid>
-        <Sidebar />
-        <EmployeeDirectory />
-      </Grid>
+      <BrowserRouter>
+        <div>
+          <Sidebar/>
+          <Route exact path="/" component={EmployeeDirectory} />
+          <Route path="/profile" component={ProfilePage} />
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
---
+++ client/src/components/profilePage.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProfilePage extends Component {
+  render() {
+    return (
+      <div>
+        Profile
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
