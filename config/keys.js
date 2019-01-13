b7fc1905b0e47477247f6e9b0b90e183b0039bc1
Gareth <gareth.kh.lau@gmail.com>
2019-01-12 20:31:16 -0500

    new mongo database

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   googleClientID: '406814365576-gm3r3pblhgkgcl46k13jd17i30h4ce9l.apps.googleusercontent.com',
   googleClientSecret: 'BWsbOTRMS-D49klTNVR5WJpk',
-  mongoURI: 'mongodb://main-dev:password1@ds249798.mlab.com:49798/directory-dev',
-  mongoUser: 'main-dev',
-  mongoPassword: 'password1',
+  mongoURI: 'mongodb://bigdeveloper:bigpassword1@ds255794.mlab.com:55794/bigusers',
+  mongoUser: 'bigdeveloper',
+  mongoPassword: 'bigpassword1',
   cookieKey: 'eoijgiuhguiqhuieghquieghuijedknsgnjkqengjkqngjknqejk', //arbitrary
   slackClientID: '484756884274.486265867267',
   slackClientSecret: 'df4bb49ced45a3771f1a1be3bb66fecc',
