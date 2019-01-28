b467e4188a85cd03714c8830685bb835b0c2b9d6
Gareth <gareth.kh.lau@gmail.com>
2019-01-28 11:42:42 -0500

    start of form

--- client/src/components/profileForm/form.jsx
+++ client/src/components/profileForm/form.jsx
@@ -1,1 +1,49 @@
 // this will be the user settings form to change profile
+// parent that will decide what elements get displayed
+
+import React, { Component } from "react";
+
+export class Form extends Component {
+
+	state = {
+		step: 1,
+		firstName: '',
+		lastName: '',
+		email: '',
+		position: ''
+	};
+
+	// move to the next step of the form
+	nextStep = () => {
+		const {step} = this.state;
+		this.setState({
+			step: step + 1
+		})
+	};
+
+	// go back a step of the form
+	prevStep = () => {
+		const {step} = this.state;
+		this.setState({
+			step: step - 1
+		})
+	};
+
+	// handle the field change
+	handleChange = (input) => (e) => {
+		this.setState({
+			[input]: e.target.value
+		})
+	};
+
+	render() {
+		return(
+			<div>
+
+
+			</div>
+		)
+	}
+}
+
+export default Form
