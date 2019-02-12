1cc7d0be5bd855bc708853bff371d2cbcc512e42
Gareth <gareth.kh.lau@gmail.com>
2019-02-12 14:29:40 -0500

    commented out interestsModal so david can work on profile

--- client/src/components/userProfile/settings/settings.jsx
+++ client/src/components/userProfile/settings/settings.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import InterestsModal from './interestsModal';
+//import InterestsModal from './interestsModal';
 
 class Settings extends Component {
 
@@ -119,7 +119,7 @@ class Settings extends Component {
 					onChange={this.updateData("interests")}
 					defaultValue={interests}
 				/>
-				<InterestsModal interests={interests}/>
+				{/*<InterestsModal interests={interests}/>*/}
 
 				<h4>Skills</h4>
 				<input
