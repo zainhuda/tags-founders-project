2d4f4a44077f899883890ca86309554793f400ee
Matthew <matthewdanics@gmail.com>
2019-03-18 16:38:14 -0400

    removed some useless (i hope) mongoose boilerplate

--- services/slack_services.js
+++ services/slack_services.js
@@ -7,71 +7,63 @@ const async = require("async");
 const importSlackUsers = (accessToken, res) => {
   axios
     .get("https://slack.com/api/users.list?token=" + accessToken)
-    .then(response => {
+    .then(async response => {
       if (response.data.ok) {
         console.log("CONNECTING TO MONGO");
 
         const members = response.data.members;
 
-        mongoose.connect(keys.mongoURI);
-        const db = mongoose.connection;
-        db.on("error", console.error.bind(console, "connection error:"));
-        db.once("open", async () => {
-          // connected
-
-          // we must generate schemas based on slack's data for mongoose
-
-          // user schema
-          const mongooseUserSchema = GenerateSchema.mongoose({
-            slackData: members[0],
-            teamData: {},
-            isConfirmed: false,
-            isInactive: false
-          });
-          const team_id = members[0].team_id.toUpperCase();
-
-          let User;
-          try {
-            User = mongoose.model(team_id);
-          } catch {
-            console.log("Model for", team_id, "did not exist, creating.");
-            User = mongoose.model(team_id, mongooseUserSchema, team_id);
-          }
-
-          console.log("starting Slack import");
-
-          for (let i = 0; i < members.length; i++) {
-            const user = createUserFromSlackMemeber(members[i], User);
-
-            try {
-              await User.findOne(
-                { "slackData.id": user.slackData.id },
-                (err, userDoc) => {
-                  if (userDoc != null) {
-                    console.log(
-                      "User already created:",
-                      user.teamData.firstName + " " + user.teamData.lastName,
-                      user.slackData.id
-                    );
-                  } else {
-                    user.save();
-                  }
-                }
-              );
-            } catch (e) {
-              console.log("ERROR:", e);
-            }
-          }
-          console.log("done for teamid", team_id);
-
-          db.collection(team_id)
-            .find()
-            .toArray(function(err, result) {
-              if (err) throw err;
-              console.log("Team Data", result);
-              res.send(result);
-            });
+        // we must generate schemas based on slack's data for mongoose
+        // user schema
+        const mongooseUserSchema = GenerateSchema.mongoose({
+          slackData: members[0],
+          teamData: {},
+          isConfirmed: false,
+          isInactive: false
         });
+        const team_id = members[0].team_id.toUpperCase();
+
+        let User;
+        try {
+          User = mongoose.model(team_id);
+        } catch {
+          console.log("Model for", team_id, "did not exist, creating.");
+          User = mongoose.model(team_id, mongooseUserSchema, team_id);
+        }
+
+        console.log("starting Slack import");
+
+        for (let i = 0; i < members.length; i++) {
+          const user = createUserFromSlackMemeber(members[i], User);
+
+          try {
+            await User.findOne(
+              { "slackData.id": user.slackData.id },
+              (err, userDoc) => {
+                if (userDoc != null) {
+                  console.log(
+                    "User already created:",
+                    user.teamData.firstName + " " + user.teamData.lastName,
+                    user.slackData.id
+                  );
+                } else {
+                  user.save();
+                }
+              }
+            );
+          } catch (e) {
+            console.log("ERROR:", e);
+          }
+        }
+        console.log("done for teamid", team_id);
+
+        db.collection(team_id)
+          .find()
+          .toArray(function(err, result) {
+            if (err) throw err;
+            console.log("Team Data", result);
+            res.send(result);
+          });
       }
     });
 };
@@ -90,61 +82,52 @@ const updateSlackUserInactivity = async accessToken => {
   const members = response.data.members;
   const team_id = response.data.members[0].team_id.toUpperCase();
 
-  mongoose.connect(keys.mongoURI);
-  const db = mongoose.connection;
-  db.on("error", console.error.bind(console, "connection error:"));
-
-  db.once("open", async () => {
-    // connected to db
-
-    // we must generate schemas based on slack's data for mongoose
-
-    // user schema
-    const mongooseUserSchema = GenerateSchema.mongoose({
-      slackData: members[0],
-      teamData: {},
-      isConfirmed: false,
-      isInactive: false
-    });
-
-    let User;
-    try {
-      User = mongoose.model(team_id);
-    } catch {
-      console.log("Model for", team_id, "did not exist, creating.");
-      User = mongoose.model(team_id, mongooseUserSchema, team_id);
-    }
-
-    for (let i = 0; i < members.length; i++) {
-      try {
-        // .exec() makes the operation a ture javascript promise
-        const userDoc = await User.findOne({
-          "slackData.id": members[i].id
-        }).exec();
-
-        if (userDoc != null) {
-          // user already exists, need to update their isInactive field
-          userDoc.slackData.deleted = members[i].deleted;
-
-          // big async to ensure that all mongoose operations are completed
-          try {
-            await userDoc.save();
-            // console.log("Success");
-          } catch (e) {
-            console.log("Error saving userDoc", e);
-          }
-        } else {
-          // user doesn't exist, create a new user with slack info and save
-          console.log("Encounted user that hasn't been added, adding");
-          const user = createUserFromSlackMemeber(members[i], User);
-          user.save();
-        }
-      } catch (e) {
-        console.log("ERROR:", e);
-      }
-    }
-    // console.log("done?");
+  // we must generate schemas based on slack's data for mongoose
+  // user schema
+  const mongooseUserSchema = GenerateSchema.mongoose({
+    slackData: members[0],
+    teamData: {},
+    isConfirmed: false,
+    isInactive: false
   });
+
+  let User;
+  try {
+    User = mongoose.model(team_id);
+  } catch {
+    console.log("Model for", team_id, "did not exist, creating.");
+    User = mongoose.model(team_id, mongooseUserSchema, team_id);
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    try {
+      // .exec() makes the operation a ture javascript promise
+      const userDoc = await User.findOne({
+        "slackData.id": members[i].id
+      }).exec();
+
+      if (userDoc != null) {
+        // user already exists, need to update their isInactive field
+        userDoc.slackData.deleted = members[i].deleted;
+
+        // big async to ensure that all mongoose operations are completed
+        try {
+          await userDoc.save();
+          // console.log("Success");
+        } catch (e) {
+          console.log("Error saving userDoc", e);
+        }
+      } else {
+        // user doesn't exist, create a new user with slack info and save
+        console.log("Encounted user that hasn't been added, adding");
+        const user = createUserFromSlackMemeber(members[i], User);
+        user.save();
+      }
+    } catch (e) {
+      console.log("ERROR:", e);
+    }
+  }
+  // console.log("done?");
 };
 
 const createUserFromSlackMemeber = (slackMember, UserSchema) => {
