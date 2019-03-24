1e8a23bc14e74e17f07c25d313f5d4b039ef72b7
Matthew <matthewdanics@gmail.com>
2019-03-24 14:51:27 -0400

    added center option

--- client/src/components/loading/loading.jsx
+++ client/src/components/loading/loading.jsx
@@ -4,8 +4,14 @@ import Loader from 'react-loaders'
 import './loading.css';
 
 const Loading = (props) => {
+
+    let className = "loader";
+    if (props.centered){
+        className += " centered-loader"
+    }
+
     return (
-        <div style={{width: props.width, height: props.height}} class="loader"></div>
+        <div style={{width: props.width, height: props.height}} className={className}/>
     )
 }
 
